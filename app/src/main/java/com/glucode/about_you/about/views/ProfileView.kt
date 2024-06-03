eace39bac63571a18cb1cda4a63b28d320995cb4
mandisikuse <mandisikuse@gmail.com>
2024-06-03 11:34:07 +0200

    Initial commits with two bug fixes

---
+++ app/src/main/java/com/glucode/about_you/about/views/ProfileView.kt
@@ -0,0 +1,13 @@
+package com.glucode.about_you.about.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ProfileView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+}
